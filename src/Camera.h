7c45ff6c229defa66979df361e2758b1f0a6d5e3
must-compute <admin@must-compute.com>
2023-11-24 20:16:03 -0500

    add camera render()

--- src/Camera.h
+++ src/Camera.h
@@ -2,38 +2,98 @@
 #define RAY_TRACER_CHALLENGE_CAMERA_H
 
 #include <cstddef>
+#include "Canvas.h"
+#include "Ray.h"
 #include "Transformations.h"
+#include "World.h"
 
+template<size_t WIDTH, size_t HEIGHT>
 class Camera {
 
 public:
-    Camera(size_t hsize_in, size_t vsize_in, double field_of_view_in);
+    explicit Camera(const double field_of_view_in) :
+            field_of_view_(field_of_view_in),
+            transform_(tf::Transform::identity()) {
+        // Calculate the pixel_size_, half_width_, and half_height_ given the field of view and h/v size.
+        const double half_view = std::tan(field_of_view_ / 2.0);
+        const auto aspect_ratio = static_cast<double>(WIDTH) / static_cast<double>(HEIGHT);
+        if (aspect_ratio >= 1.0) {
+            half_width_ = half_view;
+            half_height_ = half_view / aspect_ratio;
+        } else {
+            half_width_ = half_view * aspect_ratio;
+            half_height_ = half_view;
+        }
+        pixel_size_ = (half_width_ * 2.0) / static_cast<double>(WIDTH);
+    }
 
-    [[nodiscard]] size_t get_hsize() const;
+    [[nodiscard]] size_t hsize() const {
+        return WIDTH;
+    }
 
-    [[nodiscard]] size_t get_vsize() const;
+    [[nodiscard]] size_t vsize() const {
+        return HEIGHT;
+    }
 
-    [[nodiscard]] double get_field_of_view() const;
+    [[nodiscard]] double field_of_view() const {
+        return field_of_view_;
+    }
 
-    [[nodiscard]] const tf::Transform &get_transform() const;
+    [[nodiscard]] const tf::Transform &transform() const {
+        return transform_;
+    }
 
-    void set_transform(const tf::Transform &t);
+    void set_transform(const tf::Transform &t) {
+        transform_ = t;
+    }
 
-    [[nodiscard]] double get_pixel_size() const;
+    [[nodiscard]]     double pixel_size() const {
+        return pixel_size_;
+    }
 
-    void set_pixel_size(double pixel_size_in);
+    void set_pixel_size(double pixel_size_in) {
+        pixel_size_ = pixel_size_in;
+    }
+
+    [[nodiscard]] Ray ray_for_pixel(size_t pixel_x, size_t pixel_y) const {
+        // the offset from canvas edge to the pixel's center
+        const auto x_offset = (static_cast<double>(pixel_x) + 0.5) * pixel_size_;
+        const auto y_offset = (static_cast<double>(pixel_y) + 0.5) * pixel_size_;
+
+        // the untransformed coordinates of the pixel in world space
+        const auto world_x = half_width_ - x_offset;
+        const auto world_y = half_height_ - y_offset;
+
+        const auto pixel = transform_.inverse() * make_point(world_x, world_y, -1);
+        const auto origin = transform_.inverse() * make_point(0.0, 0.0, 0.0);
+        const auto direction = (pixel - origin).normalize();
+
+        return {origin, direction};
+    }
+
+    [[nodiscard]] Canvas<WIDTH, HEIGHT> render(const World &world) const {
+        auto image = Canvas<WIDTH, HEIGHT>{};
+
+        for (size_t y = 0; y < HEIGHT; ++y) {
+            for (size_t x = 0; x < WIDTH; ++x) {
+                const auto ray = ray_for_pixel(x, y);
+                const auto color = world.color_at(ray);
+                image.write_pixel(x, y, color);
+            }
+        }
+        return image;
+    }
+
 
 private:
-    size_t hsize;
-    size_t vsize;
-    double field_of_view;
-    tf::Transform transform;
+    double field_of_view_;
+    tf::Transform transform_;
     // The size of each pixel in world units.
-    double pixel_size;
+    double pixel_size_;
     // The size of half the canvas width in world units.
-    double half_width;
+    double half_width_;
     // The size of half the canvas height in world units.
-    double half_height;
+    double half_height_;
 };
 
 #endif //RAY_TRACER_CHALLENGE_CAMERA_H
--- tests/camera_test.cpp
+++ tests/camera_test.cpp
@@ -1,23 +1,23 @@
 #include <gtest/gtest.h>
 
 #include <numbers>
-#include <cmath>
 #include <cstddef>
 
 #include "../src/Transformations.h"
 #include "../src/Camera.h"
 #include "../src/util.h"
+#include "../src/World.h"
 
 TEST(Camera, Constructor) {
-    const size_t hsize = 160;
-    const size_t vsize = 120;
+    constexpr size_t hsize = 160;
+    constexpr size_t vsize = 120;
     const double field_of_view = std::numbers::pi / 2.0;
-    const auto c = Camera(hsize, vsize, field_of_view);
+    const auto c = Camera<hsize, vsize>(field_of_view);
 
-    EXPECT_EQ(c.get_hsize(), hsize);
-    EXPECT_EQ(c.get_vsize(), vsize);
-    EXPECT_EQ(c.get_field_of_view(), field_of_view);
-    EXPECT_EQ(c.get_transform(), tf::Transform::identity());
+    EXPECT_EQ(c.hsize(), hsize);
+    EXPECT_EQ(c.vsize(), vsize);
+    EXPECT_EQ(c.field_of_view(), field_of_view);
+    EXPECT_EQ(c.transform(), tf::Transform::identity());
 }
 
 TEST(Camera, ConstructorPixelSize) {
@@ -26,8 +26,8 @@ TEST(Camera, ConstructorPixelSize) {
         const size_t hsize = 200;
         const size_t vsize = 125;
         const double field_of_view = std::numbers::pi / 2.0;
-        const auto c = Camera(hsize, vsize, field_of_view);
-        EXPECT_TRUE(within_epsilon(c.get_pixel_size(), 0.01));
+        const auto c = Camera<hsize, vsize>(field_of_view);
+        EXPECT_TRUE(within_epsilon(c.pixel_size(), 0.01));
     }
 
     // Vertical canvas
@@ -35,7 +35,47 @@ TEST(Camera, ConstructorPixelSize) {
         const size_t hsize = 125;
         const size_t vsize = 200;
         const double field_of_view = std::numbers::pi / 2.0;
-        const auto c = Camera(hsize, vsize, field_of_view);
-        EXPECT_TRUE(within_epsilon(c.get_pixel_size(), 0.01));
+        const auto c = Camera<hsize, vsize>(field_of_view);
+        EXPECT_TRUE(within_epsilon(c.pixel_size(), 0.01));
     }
 }
+
+TEST(Camera, ConstructingRayThroughCenterOfCanvas) {
+    const auto camera = Camera<201, 101>(std::numbers::pi / 2.0);
+    const auto ray = camera.ray_for_pixel(100, 50);
+    EXPECT_EQ(ray.origin(), make_point(0.0, 0.0, 0.0));
+    EXPECT_EQ(ray.direction(), make_vector(0.0, 0.0, -1.0));
+}
+
+TEST(Camera, ConstructingRayThroughCornerOfCanvas) {
+    const auto camera = Camera<201, 101>(std::numbers::pi / 2.0);
+    const auto ray = camera.ray_for_pixel(0, 0);
+    EXPECT_EQ(ray.origin(), make_point(0.0, 0.0, 0.0));
+    EXPECT_EQ(ray.direction(), make_vector(0.66519, 0.33259, -0.66851));
+}
+
+TEST(Camera, ConstructingRayWhenCameraIsTransformed) {
+    auto camera = Camera<201, 101>(std::numbers::pi / 2.0);
+    camera.set_transform(tf::rotation_y(std::numbers::pi / 4.0) * tf::translation(0.0, -2.0, 5.0));
+    const auto ray = camera.ray_for_pixel(100, 50);
+    EXPECT_EQ(ray.origin(), make_point(0.0, 2.0, -5.0));
+    const auto loc = std::sqrt(2.0) / 2.0;
+    EXPECT_EQ(ray.direction(), make_vector(loc, 0.0, -loc));
+}
+
+TEST(Camera, RenderingWorldWithCamera) {
+    const auto world = make_default_world();
+    auto camera = Camera<11, 11>(std::numbers::pi / 2.0);
+
+    const auto from = make_point(0.0, 0.0, -5.0);
+    const auto to = make_point(0.0, 0.0, 0.0);
+    const auto up = make_vector(0.0, 1.0, 0.0);
+
+    camera.set_transform(tf::view_transform(from, to, up));
+    const auto image = camera.render(world);
+
+    EXPECT_EQ(image.pixel_at(5, 5), make_color(0.38066, 0.47583, 0.2855));
+}
+
+
+
