db43ad5073aefae6978fd4871cb253e52246c4aa
bsubei <6508762+bsubei@users.noreply.github.com>
2023-12-22 15:36:24 -0500

    move latest tests to group_test since they're really testing new functionality we added to Groups

--- tests/group_test.cpp
+++ tests/group_test.cpp
@@ -1,4 +1,7 @@
+
 #include <gtest/gtest.h>
+
+#include <numbers>
 
 #include "Group.h"
 #include "Sphere.h"
@@ -64,4 +67,48 @@ TEST(Group, IntersectingTransformedGroup) {
     const auto r = Ray{make_point(10.0, 0.0, -10.0), make_vector(0.0, 0.0, 1.0)};
     const auto xs = g->intersect(r);
     EXPECT_EQ(xs.size(), 2);
+}
+
+TEST(Sphere, ConvertingPointToWorldSpaceToObjectSpace) {
+    auto g1 = Group::make_group();
+    g1->set_transform(tf::rotation_y(std::numbers::pi / 2));
+    auto g2 = Group::make_group();
+    g2->set_transform(tf::scaling(2.0, 2.0, 2.0));
+    g1->add_child(g2);
+
+    auto s = std::make_shared<Sphere>(Sphere{});
+    s->set_transform(tf::translation(5.0, 0.0, 0.0));
+    g2->add_child(s);
+
+    EXPECT_EQ(s->world_to_object(make_point(-2.0, 0.0, -10.0)), make_point(0.0, 0.0, -1.0));
+}
+
+TEST(Sphere, ConvertingNormalFromObjectSpaceToWorldSpace) {
+    auto g1 = Group::make_group();
+    g1->set_transform(tf::rotation_y(std::numbers::pi / 2));
+    auto g2 = Group::make_group();
+    g2->set_transform(tf::scaling(1.0, 2.0, 3.0));
+    g1->add_child(g2);
+
+    auto s = std::make_shared<Sphere>(Sphere{});
+    s->set_transform(tf::translation(5.0, 0.0, 0.0));
+    g2->add_child(s);
+
+    const auto loc = std::sqrt(3.0) / 3.0;
+    EXPECT_EQ(s->normal_to_world(make_vector(loc, loc, loc)), make_vector(0.2857, 0.4286, -0.8571));
+}
+
+
+TEST(Sphere, FindingTheNormalOnAChildObject) {
+    auto g1 = Group::make_group();
+    g1->set_transform(tf::rotation_y(std::numbers::pi / 2));
+    auto g2 = Group::make_group();
+    g2->set_transform(tf::scaling(1.0, 2.0, 3.0));
+    g1->add_child(g2);
+
+    auto s = std::make_shared<Sphere>(Sphere{});
+    s->set_transform(tf::translation(5.0, 0.0, 0.0));
+    g2->add_child(s);
+
+    EXPECT_EQ(s->normal_at(make_point(1.7321, 1.1547, -5.5774)), make_vector(0.2857, 0.4286, -0.8571));
 }
--- tests/sphere_test.cpp
+++ tests/sphere_test.cpp
@@ -5,7 +5,6 @@
 #include "Material.h"
 #include "Matrix.h"
 #include "Transformations.h"
-#include "Group.h"
 
 TEST(Sphere, SphereDefaultTransformation) {
     const auto s = Sphere{};
@@ -86,48 +85,4 @@ TEST(Sphere, HelperForProducingGlassSphere) {
     EXPECT_EQ(sphere.transform(), tf::Transform::identity());
     EXPECT_EQ(sphere.material().transparency, 1.0);
     EXPECT_EQ(sphere.material().refractive_index, 1.5);
-}
-
-TEST(Sphere, ConvertingPointToWorldSpaceToObjectSpace) {
-    auto g1 = Group::make_group();
-    g1->set_transform(tf::rotation_y(std::numbers::pi / 2));
-    auto g2 = Group::make_group();
-    g2->set_transform(tf::scaling(2.0, 2.0, 2.0));
-    g1->add_child(g2);
-
-    auto s = std::make_shared<Sphere>(Sphere{});
-    s->set_transform(tf::translation(5.0, 0.0, 0.0));
-    g2->add_child(s);
-
-    EXPECT_EQ(s->world_to_object(make_point(-2.0, 0.0, -10.0)), make_point(0.0, 0.0, -1.0));
-}
-
-TEST(Sphere, ConvertingNormalFromObjectSpaceToWorldSpace) {
-    auto g1 = Group::make_group();
-    g1->set_transform(tf::rotation_y(std::numbers::pi / 2));
-    auto g2 = Group::make_group();
-    g2->set_transform(tf::scaling(1.0, 2.0, 3.0));
-    g1->add_child(g2);
-
-    auto s = std::make_shared<Sphere>(Sphere{});
-    s->set_transform(tf::translation(5.0, 0.0, 0.0));
-    g2->add_child(s);
-
-    const auto loc = std::sqrt(3.0) / 3.0;
-    EXPECT_EQ(s->normal_to_world(make_vector(loc, loc, loc)), make_vector(0.2857, 0.4286, -0.8571));
-}
-
-
-TEST(Sphere, FindingTheNormalOnAChildObject) {
-    auto g1 = Group::make_group();
-    g1->set_transform(tf::rotation_y(std::numbers::pi / 2));
-    auto g2 = Group::make_group();
-    g2->set_transform(tf::scaling(1.0, 2.0, 3.0));
-    g1->add_child(g2);
-
-    auto s = std::make_shared<Sphere>(Sphere{});
-    s->set_transform(tf::translation(5.0, 0.0, 0.0));
-    g2->add_child(s);
-
-    EXPECT_EQ(s->normal_at(make_point(1.7321, 1.1547, -5.5774)), make_vector(0.2857, 0.4286, -0.8571));
 }
